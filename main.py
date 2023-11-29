236c645040852f1ef9e7b236d1e7647da6328724
kanoki1809 <144712774+kanoki1809@users.noreply.github.com>
2023-11-29 09:21:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import io
 st.title('Data Viusalize')
 st.header('upload data file')
-st.file_uploader('choose a csv file: ', type=(['.csv']))
+ data_file = st.file_uploader('choose a csv file: ', type=(['.csv']))
 
 if data_file is not None:
   df=pd.read_csv(data_file)
