24f88e6f4953a44f623ddb5adb4bc0b8108eb0c3
kanoki1809 <144712774+kanoki1809@users.noreply.github.com>
2023-11-29 09:04:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import streamlit as st
 st.title('Data Viusalize')
 st.header('upload data file')
 st.file_uploader('choose a csv file: ', type=(['.csv']))
+
 if data_file is not None:
   df=pd.read_csv(data_file)
   
   st.header('showdata')
-  st.dataFrame(df)
+  st.dataframe(df)
 
 
 
