b5c7753f35fed22672e79df576c7021087979d83
kanoki1809 <144712774+kanoki1809@users.noreply.github.com>
2023-11-29 10:38:36 +0700

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -2,3 +2,11 @@ import streamlit as st
 from PIL import Image
 import pickle as pkl
 import numpy
+
+class_list={}
+st.title('PNEUMONIA')
+input=open('Irc_xray.pkl','rb')
+model=pkl.load(input)
+
+st.header('image')
+image = st.file_uploader('choose an image:',type=(['png'],[jpg],[jpeg])
